e410200b455d2d627d922d0b4a569af2427b6d20
Eric Galvan <lericgalvan@outlook.com>
2025-12-19 08:48:49 -0800

    Drop tables we don't want

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+
+# cvs read
+df = pd.read_csv("data/routespeeds.csv")
+
+##################################################
+# Tidy data
+###################################################
+
+
+# drop tables we don't want
+df = df.drop(
+    columns=[
+        "OBJECTID",
+        "base64_url",
+        "agency",
+        "district_name",
+        "route_name",
+        "org_id",
+    ]
+)
+
+
 def main():
-    print("Hello, World!")
+    print(df.head())
 
 if __name__ == "__main__":
     main()
