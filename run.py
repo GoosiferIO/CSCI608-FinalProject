cf7b7c8da715fcbc9b834d76e9e601211b07ca90
Eric Galvan <lericgalvan@outlook.com>
2025-12-19 09:01:23 -0800

    Group duplicate route_ids

--- run.py
+++ run.py
@@ -29,8 +29,16 @@ df = df.rename(columns={
 	"Shape_Length":"route_length",
 })
 
+# group duplicate route_id rows by taking avg speed
+avg_speed = (
+    df.groupby(["route_id", "time_period"])
+    .agg(mean_speed=("speed", "mean"))
+    .reset_index()
+)
+
+
 def main():
-    print(df.head())
+    print(avg_speed.head())
 
 if __name__ == "__main__":
     main()
